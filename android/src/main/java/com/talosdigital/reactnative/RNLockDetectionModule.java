fe0d8a7f6e94ed1d8cc0f185f2fa5a8f42e3a71e
Danh Nguyen <dnncntt@gmail.com>
2020-02-20 10:03:34 +0100

    better handle lock/unlock event

--- android/src/main/java/com/talosdigital/reactnative/RNLockDetectionModule.java
+++ android/src/main/java/com/talosdigital/reactnative/RNLockDetectionModule.java
@@ -44,10 +44,14 @@ public class RNLockDetectionModule extends ReactContextBaseJavaModule {
 
         KeyguardManager myKM = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
 
-        if (strAction.equals(Intent.ACTION_SCREEN_OFF) || strAction.equals(Intent.ACTION_SCREEN_ON)) {
+        if (strAction.equals(Intent.ACTION_SCREEN_OFF)) {
           if (myKM.inKeyguardRestrictedInputMode()) {
             sendEvent("phoneLocked", "LOCKED");
-          } else {
+          } 
+        }
+
+        if (strAction.equals(Intent.ACTION_SCREEN_ON)) {
+          if (!myKM.inKeyguardRestrictedInputMode()) {
             sendEvent("phoneUnlocked", "UNLOCKED");
           }
         }
